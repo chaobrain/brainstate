0daaad813cbec830bf30c2d7c8696a146378a996
Chaoming Wang <adaduo@outlook.com>
2025-05-18 21:54:46 +0800

    Require Python 3.10 (#75)
    * Refactor concepts-zh.ipynb to enhance clarity and structure of key concepts in BrainState framework
    
    * Update setup.py to refine package dependencies and enhance extra requirements for CPU, CUDA, and TPU support
    
    * Refactor import statements to unify Device import across modules and enhance compatibility with jax versions
    
    * Remove unnecessary future annotations imports across multiple files
    
    * Refactor type hints to use forward references for improved compatibility
    
    * Bump version to 0.1.1 and update Python requirement to >=3.10 in setup files

--- brainstate/__init__.py
+++ brainstate/__init__.py
@@ -17,7 +17,7 @@
 A ``State``-based Transformation System for Program Compilation and Augmentation
 """
 
-__version__ = "0.1.0"
+__version__ = "0.1.1"
 
 from . import augment
 from . import compile
