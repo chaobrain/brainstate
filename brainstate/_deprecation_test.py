7cb337e74f641e5f4a23dfbce613c5e399016356
oujago <oujago@gmail.com>
2025-10-04 14:56:53 +0800

    Refactor: update copyright notices across multiple files to reflect new ownership by BrainX Ecosystem Limited

--- brainstate/_deprecation_test.py
+++ brainstate/_deprecation_test.py
@@ -1,13 +1,20 @@
-#!/usr/bin/env python3
-"""
-Comprehensive tests for deprecated modules (augment, compile, functional).
+# Copyright 2024 BrainX Ecosystem Limited. All Rights Reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+# ==============================================================================
 
-Tests that:
-1. Deprecation warnings are shown appropriately
-2. All functionality is properly forwarded to replacement modules
-3. API compatibility is maintained
-4. Error handling works correctly
-"""
+# -*- coding: utf-8 -*-
+
 
 import unittest
 import warnings
--- brainstate/nn/_conv_test.py
+++ brainstate/nn/_conv_test.py
@@ -1,19 +1,25 @@
+# Copyright 2024 BrainX Ecosystem Limited. All Rights Reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+# ==============================================================================
+
 # -*- coding: utf-8 -*-
 
-import jax.numpy as jnp
-import pytest
-from absl.testing import absltest
-from absl.testing import parameterized
-
-import brainstate
 import unittest
+
 import jax
 import jax.numpy as jnp
-import brainstate
 
-
-import unittest
-import jax.numpy as jnp
 import brainstate
 
 
@@ -78,7 +84,8 @@ class TestConv1d(unittest.TestCase):
 
     def test_with_bias(self):
         """Test Conv1d with bias."""
-        conv = brainstate.nn.Conv1d(in_size=(50, 8), out_channels=16, kernel_size=3, b_init=brainstate.init.Constant(0.0))
+        conv = brainstate.nn.Conv1d(in_size=(50, 8), out_channels=16, kernel_size=3,
+                                    b_init=brainstate.init.Constant(0.0))
         x = jnp.ones((2, 50, 8))
         y = conv(x)
 
