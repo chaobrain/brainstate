f8dca4e1e0ba56da5923ee6bd47f21bcc53255dd
Chaoming Wang <adaduo@outlook.com>
2025-11-19 12:51:26 +0800

    Refactor test file: rename _parser_run_test.py to _compiler_test.py and update imports

--- brainstate/experimental/graph_ir/_compiler_test.py
+++ brainstate/experimental/graph_ir/_compiler_test.py
@@ -1,29 +1,117 @@
-# Copyright 2025 BrainX Ecosystem Limited. All Rights Reserved.
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#     http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
-# ==============================================================================
+# Test the ParsedResults.run function
 
-"""Test cases for the SNN compiler."""
+import sys
 
-import brainpy
-import braintools.init
-import brainunit as u
-import matplotlib.pyplot as plt
+sys.path.insert(0, '../gdiist_bpu')
 
 import brainstate
+import brainunit as u
+import brainpy
+import braintools
+import matplotlib.pyplot as plt
 from brainstate.experimental.graph_ir import compile_fn
 
 brainstate.environ.set(dt=0.1 * u.ms)
+
+
+def test_simple_lif_run():
+    """Test run function with a simple LIF neuron."""
+    print("\n" + "=" * 80)
+    print("Test: ParsedResults.run with Simple LIF")
+    print("=" * 80)
+
+    # Create LIF neuron
+    lif = brainpy.state.LIFRef(
+        10,
+        V_rest=-65. * u.mV,
+        V_th=-50. * u.mV,
+        V_reset=-60. * u.mV,
+        tau=20. * u.ms,
+        tau_ref=5. * u.ms,
+    )
+    brainstate.nn.init_all_states(lif)
+
+    # Define update function
+    def update(t, inp):
+        with brainstate.environ.context(t=t):
+            lif(inp)
+            return lif.get_spike()
+
+    # Parse
+    t = 0. * u.ms
+    inp = 5. * u.mA
+    parse_output = compile_fn(update)(t, inp)
+
+    result = parse_output.run(t, inp)
+    print(result)
+
+
+def test_two_populations_run():
+    """Test run function with two connected populations."""
+    print("\n" + "=" * 80)
+    print("Test: ParsedResults.run with Two Populations")
+    print("=" * 80)
+
+    class TwoPopNet(brainstate.nn.Module):
+        """ """
+
+        def __init__(self):
+            super().__init__()
+            self.n_exc = 50
+            self.n_inh = 12
+
+            self.exc = brainpy.state.LIFRef(
+                self.n_exc,
+                V_rest=-65. * u.mV,
+                V_th=-50. * u.mV,
+                V_reset=-60. * u.mV,
+                tau=20. * u.ms,
+                tau_ref=5. * u.ms,
+            )
+
+            self.inh = brainpy.state.LIFRef(
+                self.n_inh,
+                V_rest=-65. * u.mV,
+                V_th=-50. * u.mV,
+                V_reset=-60. * u.mV,
+                tau=10. * u.ms,
+                tau_ref=5. * u.ms,
+            )
+
+            self.exc2inh = brainpy.state.AlignPostProj(
+                comm=brainstate.nn.EventFixedProb(
+                    self.n_exc, self.n_inh,
+                    conn_num=0.1,
+                    conn_weight=1.0 * u.mS
+                ),
+                syn=brainpy.state.Expon.desc(self.n_inh, tau=5. * u.ms),
+                out=brainpy.state.CUBA.desc(scale=u.volt),
+                post=self.inh
+            )
+
+        def update(self, t, inp_exc, inp_inh):
+            with brainstate.environ.context(t=t):
+                exc_spk = self.exc.get_spike() != 0.
+                self.exc2inh(exc_spk)
+                self.exc(inp_exc)
+                self.inh(inp_inh)
+                return self.exc.get_spike(), self.inh.get_spike()
+
+    net = TwoPopNet()
+    brainstate.nn.init_all_states(net)
+
+    def update(t, inp_exc, inp_inh):
+        return net.update(t, inp_exc, inp_inh)
+
+    # Parse
+    t = 0. * u.ms
+    inp_exc = 5. * u.mA
+    inp_inh = 3. * u.mA
+
+    parse_output = compile_fn(update)(t, inp_exc, inp_inh)
+    result = parse_output.run(t, inp_exc, inp_inh)
+
+    print(result)
 
 
 def test_simple_lif():
